386b6dbb2c2ec6f53f276af91a99845f08e2cbee
Oleg Kalinkin <theadvinci@gmail.com>
2019-09-13 18:28:23 +0300

    Operators

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 2;
+
+            //Arithmetic Operators
+            //Multiple Line Comment
+            /*Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y);*/
+
+            Console.WriteLine(x = y);
+            x = 5;
+            y = 2;
+            Console.WriteLine(x += y);
+            x = 5;
+            y = 2;
+            Console.WriteLine(x -= y);
+            x = 5;
+            y = 2;
+            Console.WriteLine(x *= y);
+            x = 5;
+            y = 2;
+
+            // Jäägi leidmine. Mitu korda y mahub x-i sisse.
+            Console.WriteLine(x % y);
+
+
+            /*
+            //Comparison operators
+            // X = Y
+            Console.WriteLine(x == y);
+
+            // X EI = Y
+            Console.WriteLine(x != y);
+
+            // X > Y
+            Console.WriteLine(x > y);
+
+            // X > või = Y
+            Console.WriteLine(x >= y); */
+
+            //Logical operators
+            // && - AND
+            // || - OR
+            Console.WriteLine(x != y && y == 2);
+
+            Console.WriteLine(x != y || y == 10);
+
+            Console.WriteLine(!(y == 10));
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
